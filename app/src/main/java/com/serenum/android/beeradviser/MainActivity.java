10a083358a4201a6ad3b31dc92dc6b2a8bd8cf06
serenum <saveliev_@rambler.ru>
2017-04-02 14:11:55 +0600

    Create three wigets and handler

--- app/src/main/java/com/serenum/android/beeradviser/MainActivity.java
+++ app/src/main/java/com/serenum/android/beeradviser/MainActivity.java
@@ -1,7 +1,10 @@
 package com.serenum.android.beeradviser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getAdvice(View view) {
+        TextView advice = (TextView) findViewById(R.id.advice_textview);
+        Spinner beerColor = (Spinner) findViewById(R.id.beer_color);
+        if((String.valueOf(beerColor.getSelectedItem())).equals("Dark"))
+            advice.setText("Great choice, I love a dark beer!");
+            else
+            advice.setText("FUUU!!!");
     }
 }
